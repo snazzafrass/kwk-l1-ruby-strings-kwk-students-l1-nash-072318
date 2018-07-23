a91119320b724896af5bf9adbda857a8a055355f
snazzafrass <snazzafrass@gmail.com>
2018-07-23 19:59:54 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,9 +2,9 @@ puts "What is your name?"
 guest_name= gets.chomp
 
 
-puts #{party_name} = gets.chomp
-puts #{date}=gets.chomp
-time=gets.chomp
-host_name=gets.chomp
+puts #{party_name}= gets.chomp
+puts #{date}= gets.chomp
+puts #{time}= gets.chomp
+puts #{host_name}= gets.chomp
 
 puts "Hi, you've been invited to a party! What is your name?"
